7259fd261817e3e73f6fdd4488211b3a23ce1858
ANHNT\admin <anh.nt.1196@gmail.com>
2018-08-05 00:18:56 +0700

    domain entitys

---
+++ TeduShop.Model/Model/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Model
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ TeduShop.Model/Model/SupportOnline.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Model
+{
+    [Table("SupportOnlines")]
+    class SupportOnline
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+        [MaxLength(50)]
+        public string Department { set; get; }
+        [MaxLength(50)]
+        public string Skype { set; get; }
+        [MaxLength(50)]
+        public string Mobile { set; get; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(50)]
+        public string Yahoo { set; get; }
+        [MaxLength(50)]
+        public string FaceBook { set; get; }
+        [MaxLength(50)]
+        public string Status { set; get; }
+        [MaxLength(50)]
+        public int? DisplayOrder { set; get; }
+
+    }
+}
